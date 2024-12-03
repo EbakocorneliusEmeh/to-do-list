fae0414e5403deb14097148bc49356a4b0ac69f1
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2024-12-03 10:50:34 +0200

    made new work

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-const boy = document.getElementById('select')
-const girl = document.getElementById('sub')
-const list = document.getElementById('list')
+const boy = document.getElementById("select");
+const girl = document.getElementById("sub");
+const list = document.getElementById("list");
 
-girl.addEventListener('click', () => {
-  const ulitem = document.createElement('li')
-  ulitem.innerHTML = boy.value
-  list.append(ulitem)
-  input.value = ''
-})
+girl.addEventListener("click", () => {
+  const ulitem = document.createElement("li");
+  ulitem.innerHTML = boy.value;
+  list.append(ulitem);
+  input.value = "";
+});
