aadd42eafb86d4196be3c9d2bb57d98c277fd935
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2024-12-03 10:44:44 +0200

    made new work

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const boy = document.getElementById('select')
+const girl = document.getElementById('sub')
+const list = document.getElementById('list')
+
+girl.addEventListener('click', () => {
+  const ulitem = document.createElement('li')
+  ulitem.innerHTML = boy.value
+  list.append(ulitem)
+  input.value = ''
+})
